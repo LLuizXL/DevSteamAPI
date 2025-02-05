8ab6ec61c24b4409e3b68008ca5c29c96d176b58
Luiz G. <luiz.mwlo76@gmail.com>
2025-02-05 16:05:40 -0300

    Adicionado os Controladores

---
+++ DevSteamAPI/Models/Jogo.cs
@@ -0,0 +1,15 @@
+﻿namespace DevSteamAPI.Models
+{
+    public class Jogo
+    {
+        public Guid Jogoid { get; set; }
+        public string Nome { get; set; }
+        public int CLassificacao { get; set; }
+        public string? Descricao { get; set; }
+        // o ? permite inserir valores nulos
+        public string? Imagem { get; set; }
+
+        public Guid CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
+}
